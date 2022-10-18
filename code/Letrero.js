65bc37ed913c09e8fc630f412830c68686d5d217
Belen-Eliza <beluella1@gmail.com>
2022-10-18 10:33:39 -0300

    TDD iteration

--- code/Letrero.js
+++ code/Letrero.js
@@ -1,12 +1,11 @@
 function Letrero(){
+    this.encendidas=0;
     this.cantPrendidas= ()=>{
-        return 0;
+        return this.encendidas;
     }
     this.encender = (esquinaSI, esquinaID)=>{
         var cant=(esquinaID[0]-esquinaSI[0]+1)*(esquinaID[1]-esquinaSI[1]+1);
-        this.cantPrendidas=()=>{
-            return cant;
-        }
+        this.encendidas+= cant;
     }
 }
 
