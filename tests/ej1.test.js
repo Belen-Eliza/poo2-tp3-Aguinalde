42336af4980983fa8bbc86c9e37b38fbb1d1dd32
Belen-Eliza <beluella1@gmail.com>
2022-10-18 10:10:39 -0300

    TDD iteration

--- tests/ej1.test.js
+++ tests/ej1.test.js
@@ -3,3 +3,9 @@ test("Cuántas hay encendidas",()=>{
     letrero = new Letrero();
     expect(letrero.cantPrendidas()).toBe(0);
 });
+
+test("Cuántas hay encendidas después de prender una",()=>{
+    letrero = new Letrero();
+    letrero.encender("0,0")
+    expect(letrero.cantPrendidas()).toBe(0);
+});
