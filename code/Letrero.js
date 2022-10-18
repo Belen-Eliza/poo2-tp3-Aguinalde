7c7059eb5ac3222a6e48da1d5a3da0059eb5447f
Belen-Eliza <beluella1@gmail.com>
2022-10-18 10:26:27 -0300

    TDD iteration

--- code/Letrero.js
+++ code/Letrero.js
@@ -2,9 +2,10 @@ function Letrero(){
     this.cantPrendidas= ()=>{
         return 0;
     }
-    this.encender = ()=>{
+    this.encender = (esquinaSI, esquinaID)=>{
+        var cant=(esquinaID[0]-esquinaSI[0]+1)*(esquinaID[1]-esquinaSI[1]+1);
         this.cantPrendidas=()=>{
-            return 1;
+            return cant;
         }
     }
 }
--- tests/ej1.test.js
+++ tests/ej1.test.js
@@ -6,12 +6,12 @@ test("Cuántas hay encendidas",()=>{
 
 test("Cuántas hay encendidas después de prender una",()=>{
     var letrero = new Letrero();
-    letrero.encender("0,0");
+    letrero.encender([0,0],[0,0]);
     expect(letrero.cantPrendidas()).toBe(1);
 });
 
 test("Cuántas hay encendidas después de prender un cuadrado de 2x2",()=>{
     var letrero = new Letrero();
-    letrero.encender("0,0","1,1");
+    letrero.encender([0,0],[1,1]);
     expect(letrero.cantPrendidas()).toBe(4);
 });
