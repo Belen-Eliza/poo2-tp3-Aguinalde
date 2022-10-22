383182e9edc0237ca7a66ff41a61f3c93d60721a
Belen-Eliza <beluella1@gmail.com>
2022-10-22 16:39:23 -0300

    TDD iteration

--- tests/ej2.test.js
+++ tests/ej2.test.js
@@ -23,3 +23,11 @@ test("Subir intensidad dos veces",()=>{
     lampara.prender();
     expect(lampara.intensidad).toBe(2);
 })
+
+test("Subir intensidad mas de 10 veces",()=>{
+    let lampara=new Lampara();
+    for (let k=0; k<11;k++){
+        lampara.prender();
+    }
+    expect(lampara.intensidad).toBe(10);
+})
