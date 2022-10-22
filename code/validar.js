0a31ffcb4aad4f5a8b1895d70dbb4cd6438ec35e
Belen-Eliza <beluella1@gmail.com>
2022-10-22 16:09:47 -0300

    TDD iteration

--- code/validar.js
+++ code/validar.js
@@ -1,4 +1,9 @@
-function validarCuadrado(){
-    throw new Error("Cuadrado invalido");
+function validarCuadrado(esquinaSI,esquinaID){
+    let filas = esquinaID[0]-esquinaSI[0];
+    let columnas=esquinaID[1]-esquinaSI[1];
+    if (filas<0 || columnas<0){
+        throw new Error("Cuadrado invalido");
+    }
+    
 }
 module.exports =validarCuadrado;
